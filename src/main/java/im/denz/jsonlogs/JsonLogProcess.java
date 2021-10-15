82af6587ce531abb957893b986c33e56bcc629e0
Denys Zatsaynnyy <Denys.Zatsarynnyy@walmart.com>
2021-10-15 10:08:07 -0500

    add rx impl

--- src/main/java/im/denz/jsonlogs/JsonLogProcess.java
+++ src/main/java/im/denz/jsonlogs/JsonLogProcess.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ForkJoinPool;
@@ -17,16 +22,18 @@ import java.util.stream.Stream;
 @Slf4j
 public class JsonLogProcess {
 
+    public static final int FLUX_WINDOWS_SIZE = 100;
+
     public static final String KEY_FILED = "nm";
 
     public static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static final ForkJoinPool pool = ExecutorConfig.factoryPool(ExecutorConfig.MIN_THREADS_COUNT,"json-logs");
+    public static final ForkJoinPool pool = ExecutorConfig.factoryPool(ExecutorConfig.MIN_THREADS_COUNT, "json-logs");
 
     public static KeyCountResult parseFile(String fileName) {
         AtomicReference<KeyCountResult> result = new AtomicReference<>(new KeyCountResult());
         try (Stream<String> is = Files.lines(Paths.get(fileName))) {
-            is.map(line -> CompletableFuture.runAsync( () -> {
+            is.map(line -> CompletableFuture.runAsync(() -> {
                 try {
                     Map<String, String> jsonMap = objectMapper.readValue(line, new TypeReference<Map<String, String>>() {
                     });
@@ -40,6 +47,40 @@ public class JsonLogProcess {
         } catch (IOException ex) {
             log.error(ex.getLocalizedMessage(), ex);
         }
+        return result.get();
+    }
+
+    public static KeyCountResult parseFileRx(String fileName) {
+        AtomicReference<KeyCountResult> result = new AtomicReference<>(new KeyCountResult());
+
+        Path path = Paths.get(fileName);
+        Scheduler scheduler = Schedulers.fromExecutor(pool);
+        Flux.using(
+                        () -> Files.lines(path),
+                        Flux::fromStream,
+                        Stream::close
+                )
+                .window(FLUX_WINDOWS_SIZE)
+                .flatMap(w -> w.reduce(new ArrayList<String>(), (a, b) -> {
+                    a.add(b);
+                    return a;
+                }))
+                .publishOn(scheduler)
+                .doOnNext(list -> {
+                    list.forEach(line -> {
+                        try {
+                            Map<String, String> jsonMap = objectMapper.readValue(line, new TypeReference<Map<String, String>>() {
+                            });
+                            if (jsonMap.containsKey(KEY_FILED)) {
+                                result.get().add(jsonMap.get(KEY_FILED));
+                            }
+                        } catch (JsonProcessingException ex) {
+                            log.debug(ex.getMessage());
+                        }
+                    });
+
+                }).then().block();
+
         return result.get();
     }
 }
--- src/test/java/im/denz/jsonlogs/JsonLogProcessTest.java
+++ src/test/java/im/denz/jsonlogs/JsonLogProcessTest.java
@@ -2,7 +2,7 @@ package im.denz.jsonlogs;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JsonLogProcessTest {
 
@@ -19,6 +19,22 @@ class JsonLogProcessTest {
     void parseFile_L5_Test() throws Exception{
         String fileName = "logs5.txt";
         KeyCountResult result = JsonLogProcess.parseFile(TestHelper.getFixturePath(fileName));
+        assertEquals(2,result.getExtCount("pdf"));
+        assertEquals(2,result.getExtCount("ext"));
+    }
+
+    @Test
+    void parseFile_L3_RX_Test() throws Exception{
+        String fileName = "logs3.txt";
+        KeyCountResult result = JsonLogProcess.parseFileRx(TestHelper.getFixturePath(fileName));
+        assertEquals(1,result.getExtCount("pdf"));
+        assertEquals(1,result.getExtCount("ext"));
+    }
+
+    @Test
+    void parseFile_L5_RX_Test() throws Exception{
+        String fileName = "logs5.txt";
+        KeyCountResult result = JsonLogProcess.parseFileRx(TestHelper.getFixturePath(fileName));
         assertEquals(2,result.getExtCount("pdf"));
         assertEquals(2,result.getExtCount("ext"));
     }
